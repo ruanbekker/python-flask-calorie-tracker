a250413cafed448d9dfeddc9d264561e87b0d134
Ruan Bekker <ruanbekker@users.noreply.github.com>
2025-06-20 13:29:13 +0000

    :sparkles: add mysql support

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
+import os
+import pymysql
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+pymysql.install_as_MySQLdb()
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///calories.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:///calories.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
